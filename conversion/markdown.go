df5c337e37c2f7e16cd6f0747b4cb94bf6db3b76
Tom Kwok <contact@tomkwok.com>
2015-07-21 21:30:18 +0800

    Enable blackfriday footnotes extension

--- conversion/markdown.go
+++ conversion/markdown.go
@@ -6,6 +6,26 @@ import (
 
 // Using blackfriday library for markdown to html conversion. At least for now.
 
+const (
+	htmlFlags = 0 |
+		// We don't want blackfriday.HTML_USE_XHTML
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES |
+		blackfriday.HTML_FOOTNOTE_RETURN_LINKS
+
+	extensions = 0 |
+		blackfriday.EXTENSION_TABLES |
+		blackfriday.EXTENSION_FENCED_CODE |
+		blackfriday.EXTENSION_AUTOLINK |
+		blackfriday.EXTENSION_STRIKETHROUGH |
+		blackfriday.EXTENSION_SPACE_HEADERS |
+		blackfriday.EXTENSION_HEADER_IDS |
+		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
+		blackfriday.EXTENSION_FOOTNOTES
+)
+
 func GenerateHtmlFromMarkdown(input []byte) []byte {
-	return blackfriday.MarkdownCommon(input)
+	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+	return blackfriday.Markdown(input, renderer, extensions)
 }
